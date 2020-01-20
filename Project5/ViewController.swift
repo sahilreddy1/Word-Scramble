3cb03460da15f294426d5401a3de3ce26fe19ee6
Sahil Reddy <sahilpreddy96@gmail.com>
2020-01-19 19:32:22 -0600

    First commit

--- Project5/ViewController.swift
+++ Project5/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(startGame))
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
         
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
@@ -29,7 +29,7 @@ class ViewController: UITableViewController {
         startGame()
     }
 
-    func startGame(){
+    @objc func startGame(){
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
@@ -57,15 +57,18 @@ class ViewController: UITableViewController {
         ac.addAction(submitAction)
         present(ac, animated: true)
     }
+    
     func submit(_ answer: String) {
         let lowerAnswer = answer.lowercased()
         
         let errorTitle: String
         let errorMessage: String
         
+        
         if isPossible(word: lowerAnswer) {
             if isOriginal(word: lowerAnswer) {
                 if isReal(word: lowerAnswer) {
+                    
                     usedWords.insert(answer, at: 0)
                     
                     let indexPath = IndexPath(row: 0, section: 0)
@@ -73,22 +76,23 @@ class ViewController: UITableViewController {
                     
                     return
                 } else {
-                    errorTitle = "Word not real"
-                    errorMessage = "Made up word."
+                    (errorTitle,errorMessage) = showErrorMessage(errorTitle: "Word not real", errorMessage:"Made up word.")
+                    
                 }
             } else {
-                errorTitle = "Word already used"
-                errorMessage = "You need to use a word not used."
+                (errorTitle,errorMessage) = showErrorMessage(errorTitle: "Word already used. Be original!", errorMessage: "You need to use a word not used.")
+                
             }
         } else {
-            errorTitle = "Word not possible"
-            errorMessage = "Cannot spell that word from \(title!.lowercased())."
+            (errorTitle,errorMessage) = showErrorMessage(errorTitle: "Word not possible", errorMessage: "Cannot spell that word from \(title!.lowercased()).")
+            
         }
         let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
         present(ac, animated: true)
     }
     
+    //Check to see if the word can be formed from the given title word
     func isPossible(word: String) -> Bool {
         guard var tempWord = title?.lowercased() else{return false}
         
@@ -102,15 +106,25 @@ class ViewController: UITableViewController {
         return true
     }
     
+    //has the word been used before or is it the title
     func isOriginal(word: String) -> Bool {
+        if word == title {
+            return false
+        }
         return !usedWords.contains(word)
     }
     
+    //is it a real word
     func isReal(word: String) -> Bool {
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
         return misspelledRange.location == NSNotFound
+    }
+    
+    //shows users error and tells them to submit a new one
+    func showErrorMessage(errorTitle: String, errorMessage: String) -> (String, String){
+        return ( errorTitle, errorMessage)
     }
 }
 
